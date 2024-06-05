0e4cd9dedd24c8e3d2ffe6bb8dc6d42a362e6ca2
Mark Greenberg <magreenberg@gmail.com>
2024-06-05 08:54:26 -0500

    Initial Commit

---
+++ GitHub_Demo/GitHub_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_DemoApp.swift
+//  GitHub_Demo
+//
+//  Created by Mark Greenberg on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
